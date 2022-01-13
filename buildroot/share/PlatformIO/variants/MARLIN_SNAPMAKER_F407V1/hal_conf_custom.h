24da6f7e7a902c7e0b9f7e0f201500933b401c4e
Scott Huang <shili.huang@snapmaker.com>
2022-01-13 10:39:17 +0800

    Fix: change crystal to 25MHz
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- buildroot/share/PlatformIO/variants/MARLIN_SNAPMAKER_F407V1/hal_conf_custom.h
+++ buildroot/share/PlatformIO/variants/MARLIN_SNAPMAKER_F407V1/hal_conf_custom.h
@@ -89,7 +89,7 @@ extern "C" {
   *        (when HSE is used as system clock source, directly or through the PLL).
   */
 #ifndef HSE_VALUE
-#define HSE_VALUE              8000000U /*!< Value of the External oscillator in Hz */
+#define HSE_VALUE              25000000U /*!< Value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
 #ifndef HSE_STARTUP_TIMEOUT

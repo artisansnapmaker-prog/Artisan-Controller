ebc7232a59ee9efc304f98657d79efd5c0627f96
Scott Huang <shili.huang@snapmaker.com>
2022-03-08 20:29:22 +0800

    Feature: add API to register subscription callback
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- snapmaker/src/host/sacp_hmi.h
+++ snapmaker/src/host/sacp_hmi.h
@@ -27,7 +27,9 @@
 
 #define SACP_HMI_LINK_MAX (2)
 
-typedef err_code_t (*sacp_hmi_callback)(void *obj, sacp_hmi_message_t *);
+typedef err_code_t (*sacp_hmi_callback)(void *obj, sacp_hmi_message_t *msg);
+
+typedef uint16_t (*sacp_hmi_subscribe_callback)(void *obj, uint8_t *buffer);
 
 typedef struct {
   void *obj;
@@ -46,6 +48,8 @@ class HostSACPHMI: public HostSACP {
     err_code_t init(TaskHandle_t event_task, SemaphoreHandle_t recv_signal);
 
     err_code_t register_callback(uint8_t cmd_set, uint8_t cmd_id, void *obj, sacp_hmi_callback cb, uint32_t attr=0);
+    err_code_t register_subscription(uint8_t cmd_set, uint8_t cmd_id, void *obj, sacp_hmi_subscribe_callback cb);
+
     err_code_t send_sync(sacp_hmi_message_t *message, uint8_t *out, uint16_t *out_len, uint32_t timeout=100, uint8_t retry=1);
     err_code_t send(sacp_hmi_message_t *in);
 

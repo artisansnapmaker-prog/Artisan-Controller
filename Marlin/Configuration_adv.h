775cba36100aafae614907edea60bf3bb0bc664a
Scott Huang <shili.huang@snapmaker.com>
2022-03-24 19:32:51 +0800

    Improve: change the HYBRID_THRESHOLD of Z axes to 50mm/s
        to eliminate the abnormal noise when z moving at 40mm/s
    
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3017,8 +3017,8 @@
   #define X2_HYBRID_THRESHOLD    200
   #define Y_HYBRID_THRESHOLD     200
   #define Y2_HYBRID_THRESHOLD    200
-  #define Z_HYBRID_THRESHOLD     40
-  #define Z2_HYBRID_THRESHOLD    40
+  #define Z_HYBRID_THRESHOLD     50
+  #define Z2_HYBRID_THRESHOLD    50
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
   #define I_HYBRID_THRESHOLD       3

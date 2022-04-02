38cb5992b98ad47432df0cc17e8f05e8a9d28ae5
Scott Huang <shili.huang@snapmaker.com>
2022-04-02 10:00:24 +0800

    Improve: change the HYBRID_THRESHOLD to 250mm/s for TMC2209 in X,Y,Y2
        To avoid abnormal motor vibration per the test from ME
    
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3013,10 +3013,10 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     220  // [mm/s]
-  #define X2_HYBRID_THRESHOLD    220
-  #define Y_HYBRID_THRESHOLD     220
-  #define Y2_HYBRID_THRESHOLD    220
+  #define X_HYBRID_THRESHOLD     250  // [mm/s]
+  #define X2_HYBRID_THRESHOLD    250
+  #define Y_HYBRID_THRESHOLD     250
+  #define Y2_HYBRID_THRESHOLD    250
   #define Z_HYBRID_THRESHOLD     50
   #define Z2_HYBRID_THRESHOLD    50
   #define Z3_HYBRID_THRESHOLD      3

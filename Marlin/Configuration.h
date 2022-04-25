a09e8b0756838c4377606291e77de3af3b74d5ee
Scott Huang <shili.huang@snapmaker.com>
2022-04-25 20:16:18 +0800

    Feature: change the lead of E to 137
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -935,7 +935,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 400, 888.89, 119.84 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 400, 888.89, 137 }
 
 /**
  * Default Max Feed Rate (mm/s)

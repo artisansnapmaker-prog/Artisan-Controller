8e4e177333c6cdd26219f7e1c627ece6308d782c
lixiaolong <786503628@qq.com>
2022-05-16 15:48:49 +0800

    Style: delete useless code

--- snapmaker/src/module/toolhead_fdm.cpp
+++ snapmaker/src/module/toolhead_fdm.cpp
@@ -756,8 +756,6 @@ static void fdm_callback_hotend_type(void *obj, uint8_t *data, uint8_t length) {
 static void fdm_callback_extruder_info(void *obj, uint8_t *data, uint8_t length) {
   ToolHeadFDM &fdm = *(ToolHeadFDM *)obj;
   fdm.report_extruder_info(data);
-
-  (void)fdm;
 }
 
 static void fdm_callback_report_hotend_offset(void *obj, uint8_t *data, uint8_t length) {

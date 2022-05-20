f839fef751b46a2072ac296cb4e90425edc6a739
zhangquanyi <zhangquanyi@snapmaker.com>
2022-05-20 21:29:26 +0800

    Feature: version change to V0.5.0-beta1-0520

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "V0.4.0-beta1-0429"
+  #define SHORT_BUILD_VERSION "V0.5.0-beta1-0520"
 #endif
 
 /**

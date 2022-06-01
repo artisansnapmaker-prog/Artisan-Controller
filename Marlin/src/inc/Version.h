f68adef090ab64273ad6b04cfde96e123283506c
lixiaolong <786503628@qq.com>
2022-06-01 21:16:08 +0800

    Feature: update version to V0.5.8

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "V0.5.7-beta1-0527"
+  #define SHORT_BUILD_VERSION "V0.5.8-beta1-0601"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2022-05-27"
+  #define STRING_DISTRIBUTION_DATE "2022-06-01"
 #endif
 
 /**

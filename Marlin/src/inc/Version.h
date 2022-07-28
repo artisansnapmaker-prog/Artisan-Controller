c04c406b33b21f72fae60926c96857f8f1f8d67a
Scott Huang <shili.huang@snapmaker.com>
2022-07-28 14:30:07 +0800

    Feature: update version to V0.6.5-0728
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "V0.6.4-0728"
+  #define SHORT_BUILD_VERSION "V0.6.5-0728"
 #endif
 
 /**

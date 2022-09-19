5494e6083cb972df6f9169e4ebca1ba7942d34b9
zhangquanyi <zhangquanyi@snapmaker.com>
2022-09-19 11:26:56 +0800

    Feature: update version to V0.7.15

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "V0.7.14-0917"
+  #define SHORT_BUILD_VERSION "V0.7.15-0917"
 #endif
 
 /**

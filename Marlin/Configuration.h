53a749f3ed0181072cbae839e7547f36350d1e81
zhangquanyi <zhangquanyi@snapmaker.com>
2022-11-24 14:52:34 +0800

    Feature: update M201 M203 M204 parameters

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -946,11 +946,11 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 250, 250, 40, 250, 45, 80 }
+#define DEFAULT_MAX_FEEDRATE          { 250, 250, 40, 250, 45, 25 }
 
 #define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 250, 250, 40, 250, 45, 80 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 250, 250, 40, 250, 45, 30 } // ...or, set your own edit limits
 #endif
 
 /**
@@ -959,7 +959,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 500, 500, 500, 6000 }
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 500, 500, 6000 }
 
 #define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -975,7 +975,7 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  5000    // E acceleration for retracts
+#define DEFAULT_RETRACT_ACCELERATION  2000    // E acceleration for retracts
 #define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
 
 #define LIMITED_MAX_STARTING_ACCEL_EDITING

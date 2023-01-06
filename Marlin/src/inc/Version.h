bfd2b86461461dd9aff7700d36ff496856218e4d
zhangquanyi <zhangquanyi@snapmaker.com>
2023-01-06 18:46:14 +0800

    Feature: update version to v2.2.11

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "V2.2.10-1230"
+  #define SHORT_BUILD_VERSION "V2.2.11-0106"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2022-12-30"
+  #define STRING_DISTRIBUTION_DATE "2022-01-06"
 #endif
 
 /**

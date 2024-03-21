68a2b6bd239c62e2858f657974b4d517fe37a254
huang-w-y <15768623511@163.com>
2024-03-21 15:30:50 +0800

    update version to V2.3.0

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -25,7 +25,7 @@
  * Release version. Leave the Marlin version or apply a custom scheme.
  */
 #ifndef SHORT_BUILD_VERSION
-  #define SHORT_BUILD_VERSION "V2.2.34-0425"
+  #define SHORT_BUILD_VERSION "V2.3.0-0321"
 #endif
 
 /**
@@ -42,7 +42,7 @@
  * version was tagged.
  */
 #ifndef STRING_DISTRIBUTION_DATE
-  #define STRING_DISTRIBUTION_DATE "2024-04-25"
+  #define STRING_DISTRIBUTION_DATE "2024-03-21"
 #endif
 
 /**

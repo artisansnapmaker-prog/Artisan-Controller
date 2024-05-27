4903a678c883d7dcd368d790d579b1bcb435fe7c
huang-w-y <15768623511@163.com>
2024-05-27 14:22:56 +0800

    Improvement: Fix: When the fan is in to_be_closed state, if the fan is manually turned off, the fan cannot be turned on the next time the job is started.

--- snapmaker/src/module/toolhead_laser.cpp
+++ snapmaker/src/module/toolhead_laser.cpp
@@ -1179,19 +1179,8 @@ void ToolHeadLaser::can_cb_handle_security_status(void *obj, uint8_t *data, uint
     }
 
     if (new_state & SAFETY_STATE_BIT_FAN_RUN) {
-      /* Due to the presence of the TEC on the module, we need to ensure that the fan on the module is working properly, 
-         otherwise there is a risk of damage due to excessive temperature rise on the module.
-         Therefore, when an abnormality is detected in the fan, we need to power off the module.
-       */
-      if (MODULE_DEVICE_ID_LASER_RED_2W_2023 == laser.get_device_id()) {
-        system_svc.raise_exception_async(laser.get_device_id(), LASER_EXCEP_STA_FAN_RUN, EXCEP_ACT_PAUSE_WORKING | EXCEP_ACT_DISABLE_POWER_8P_TOOLHEAD,
-                                          EXCEP_BAN_ENABLE_POWER_8P_TOOLHEAD | EXCEP_BAN_TURN_ON_LASER | EXCEP_BAN_WORKING);
-      }
-      else {
-        system_svc.raise_exception_async(laser.get_device_id(), LASER_EXCEP_STA_FAN_RUN, EXCEP_ACT_PAUSE_WORKING,
-                                          EXCEP_BAN_TURN_ON_LASER | EXCEP_BAN_WORKING);
-      }
-
+      system_svc.raise_exception_async(laser.get_device_id(), LASER_EXCEP_STA_FAN_RUN, EXCEP_ACT_PAUSE_WORKING,
+                                        EXCEP_BAN_TURN_ON_LASER | EXCEP_BAN_WORKING);
     }
 
     if (new_state & SAFETY_STATE_BIT_FIRE_DECT) {
@@ -1484,6 +1473,7 @@ void ToolHeadLaser::check_fan(uint16_t new_power_pwm) {
   case LASER_FAN_STATE_TO_BE_CLOSED:
     if (new_power_pwm > 0) {
       fan_state = LASER_FAN_STATE_OPEN;
+      set_fan(255);
       fan_tick  = 0;
     }
     break;

4cb17c8d8f83ad92157ea5f7d6e5715fdbc56b8b
huang-w-y <15768623511@163.com>
2024-06-15 10:07:08 +0800

    Improvement: Adjusting the maximum speed of the B-axis

--- snapmaker/src/snapmaker.cpp
+++ snapmaker/src/snapmaker.cpp
@@ -506,7 +506,7 @@ static void system_thread(void *p) {
   }
 
   // forced update of speed parameters
-  motion_platform_svc.run_gcode((char*)"M203 X200 Y200 Z40 B25");
+  motion_platform_svc.run_gcode((char*)"M203 X200 Y200 Z40 B45");
   motion_platform_svc.run_gcode((char*)"M201 X2000 Y2000 Z500");
   motion_platform_svc.run_gcode((char*)"M205 V5");
   motion_platform_svc.run_gcode((char*)"M204 S1000");
